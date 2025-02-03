a0040861e57bf20ea6a5db2923573a3726102896
Bia-source <bia_ferreirad@yahoo.com>
2025-02-03 19:54:04 -0300

    document.write explicando

--- A18-M1-crucilandia/script.js
+++ A18-M1-crucilandia/script.js
@@ -1,1 +1,23 @@
 console.log("ola mundo")
+
+// declaração funcao
+//             parametros: o que estou recebendo
+function somar(num1, num2){
+    return num1 + num2;
+}
+
+// chamando a função
+//    argumentos: o que estou passando
+// somar(3,40);
+// somar(32,40);
+// somar(30,40);
+// somar(89,40);
+// somar(8963267637,40);
+
+// var numero = String(23)
+// var nome = "Beatriz" 
+// console.log(nome.toLowerCase());
+
+// document = html
+// document.write = escrevendo no HTML
+document.write(somar(3,9));
