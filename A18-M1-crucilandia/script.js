76ba5790847604879276cd874225705b2d2f80ad
Bia-source <bia_ferreirad@yahoo.com>
2025-02-04 20:03:20 -0300

    explicação sobre arrays

--- A18-M1-crucilandia/script.js
+++ A18-M1-crucilandia/script.js
@@ -2,9 +2,9 @@ console.log("ola mundo")
 
 // declaração funcao
 //             parametros: o que estou recebendo
-function somar(num1, num2){
-    return num1 + num2;
-}
+// function somar(num1, num2){
+//     return num1 + num2;
+// }
 
 // chamando a função
 //    argumentos: o que estou passando
@@ -20,14 +20,41 @@ function somar(num1, num2){
 var teste = 90
 // document = html
 // document.write = escrevendo no HTML
-//document.write(`<h1 style="color: yellow">${somar(23,5)}</h1>`);// saida 
-//prompt // entrada
+//document.write(`<h1 class="title">${somar(23,5)}</h1>`);// saida 
+
+prompt // entrada
+// 1. criar uma funcao que recebe como parametro nome completo
+// 2. funçao deve EXIBIR no HTML 
+//    a. o nome completo do usuario 
+//    b. a quantidade total de letras no nome (funçao nativa js)
 
 // INTERPOLAÇÃO -> UNIR VARIAVEIS COM STRING
 // adicionar crase `` dentro das crases quando for utilizar
 // variaveis ou funções ${variavel/funcao}
 // ex: `<h1>${nomeUsuario}</h1>
-var nome = "Beatriz";
-var nomeJogo = "CS"
-console.log("ola " + nome + " seja bem vindo " + nomeJogo) // concatenando
-console.log(`ola ${nome} seja bem vindo ${nomeJogo}`) // interpolação
+// var nome = "Beatriz";
+// var nomeJogo = "CS"
+// console.log("ola " + nome + " seja bem vindo " + nomeJogo) // concatenando
+// console.log(`ola ${nome} seja bem vindo ${nomeJogo}`) // interpolação
+
+
+
+
+// document.getElementById("title").innerHTML = `ola ${nome} seja bem vindo ${nomeJogo}`
+
+
+
+function somar(num1, num2){
+    return num1 + num2;
+}
+
+// entrada de dados com prompt
+var numero1 = parseInt(prompt("digite o primeiro valor"));
+var numero2 = parseInt(prompt("digite o segundo valor"));
+
+// pegando o HTML, buscando dentro do HTML um id="resultado"
+// substituindo(innerHTML/innerText) o texto pelo resultado da função somar()
+document.getElementById("resultado").innerHTML = somar(numero1,numero2);
+document.getElementById("resultado").style.color = "purple";
+document.getElementById("resultado").style.color = "purple";
+document.getElementById("resultado").style.fontSize = "90px";
---
+++ A19-M1-Crucilandia/script.js
@@ -0,0 +1,72 @@
+// VARIAVEIS -> um lugar que armazena um unico valor variavel
+var nome = "Beatriz" // tipo string ""
+var idade = 24 // tipo number
+var almocou = true // tipo boolean (verdadeiro/falso)
+
+// alterando valor de variaveis
+nome = "Joao Miguel"
+idade = 18;
+almocou = false
+
+// CHAVES {} - bloco de código 
+// COLCHETES [] -> conjunto/lista de valores
+
+// VETORES - armazena uma coleção/conjunto/lista de elementos do mesmo tipo.
+// VETORES - lista de valores do mesmo tipo 
+// VETORES - []
+// VETORES - ARRAYS
+// 5mb
+var nome1 = "ana clara";
+// 5mb
+var nome2 = "ana vitoria";
+// 5mb
+var nome3 = "aniel";
+
+// CATEGORIAS -> JUNTAR VALORES PARECIDO varios nomes CATEGORIA NOMES 
+// CATEGORIAS -> JUNTAR VALORES PARECIDO varios idades de alunos CATEGORIA IDADE DOSS ALUNOS
+
+// 10mb
+var listaNomes = ["ana clara","ana vitoria", "aniel"];
+var listaIdades = [23,25,29];
+var listaSalgado = [true,false,true,true];
+
+// SENHAS -> INDEX -> INDICE -> POSIÇÃO
+//      INDEX        0        1   
+var listaNome = ["beatriz", "tabs"]
+// substituindo valores
+// listaNome[0] = "Joao Miguel"
+// console.log(listaNome)
+
+// // nomeArray.push(item) – Adiciona um item ao final do array.
+// listaNome.push("Marcilene")
+// console.log(listaNome)
+
+// // nomeArray.pop() – Remove o último item do array.
+// listaNome.pop();
+// console.log(listaNome)
+
+// // nomeArray.unshift(item) – Adiciona um item no início do array.
+// listaNome.unshift("Lauanda");
+// console.log(listaNome);
+
+// // nomeArray.shift() – Remove o primeiro item do array
+// listaNome.shift();
+// console.log(listaNome);
+
+var listaMusicasFesta = [];
+
+var nomeMusica = prompt("digite uma musica");
+var nomeMusica1 = prompt("digite uma musica");
+var nomeMusica2 = prompt("digite uma musica");
+var nomeMusica3 = prompt("digite uma musica");
+
+listaMusicasFesta.push(nomeMusica,nomeMusica1, nomeMusica2, nomeMusica3)
+console.log(listaMusicasFesta);
+
+
+
+
+
+
+
+
