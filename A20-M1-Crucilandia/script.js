179ac7dbcbfe66787e55fd5574f72822c9a0292f
Bia-source <bia_ferreirad@yahoo.com>
2025-02-05 20:43:13 -0300

    explicacao laco for

--- A20-M1-Crucilandia/script.js
+++ A20-M1-Crucilandia/script.js
@@ -7,7 +7,7 @@ var listaMusicasFesta = [];
 // var nomeMusica3 = prompt("digite uma musica");
 
 // LAÇOS DE REPETIÇÃO -> ESTRUTURA DE CONTROLE
-var count = 1;
+// var count = 1;
 
 // // enquanto(condição for verdadeira){
 // //    faça isso 
@@ -19,27 +19,87 @@ var count = 1;
 // }
 // console.log(listaMusicasFesta)
 
-var listaNomes = ["Beatriz", "Kauanny", "Luisa"];
+// var listaNomes = ["Beatriz", "Kauanny", "Luisa"];
 // console.log(listaNomes[0]);
 // console.log(listaNomes[1]);
 // console.log(listaNomes[2]);
 
 // condição -> quantidade de vezes que queremos 
 // repetir o mesmo código
-var vezes = 0;
-while(vezes < 3){
-    // código que queremos repetir 
-    document.write(`incrivel divas ${listaNomes[vezes]} <br>`);
-    // atualização do valor vezes
-    vezes = vezes + 1;
+// var vezes = 0;
+// while(vezes < 3){
+//     // código que queremos repetir 
+//     document.write(`incrivel divas ${listaNomes[vezes]} <br>`);
+//     // atualização do valor vezes
+//     vezes = vezes + 1;
+// }
+
+// var numero = 0;
+
+// while(numero < 5){
+//     document.write(`incrivel <br>`);
+//     numero = numero + 1;
+// }
+
+
+
+// console.log(listaNomes[0]);
+// console.log(listaNomes[1]);
+// console.log(listaNomes[2]);
+
+// LISTAR -> FOR 
+// LISTAR -> MOSTRAR LISTA NA TELA
+// LISTAR - PERCORRER UM ARRAY
+// PERCORRER ARRAY - OLHAR TODOS OS ITENS DENTRO DO ARRAY
+
+// index++  ->  index = index + 1;
+//  index= senha;enquanto SENHA <= quantidade maxima de itens; incremento 
+// var index = 0 -> variavel contadora
+// index <= listaNomes.length -> condição 
+// index++ -> incremento
+var listaNomes = ["Beatriz", "Kauanny", "Luisa", "Beatriz", "Kauanny", "Luisa", ];
+for(var index = 0; index < listaNomes.length; index++){
+    document.write(`incrivel divas ${listaNomes[index]} <br>`)
+}
+var numeros = [24,432, 9854, 844, 564];
+for(var i = 0; i < numeros.length; i++){
+    document.write(`<li style="color: blue">${numeros[i]}</li>`)
 }
 
-var numero = 0;
-
-while(numero < 5){
-    document.write(`incrivel <br>`);
-    numero = numero + 1;
+var produtos = ["Notebook", "Smartphone", "Mouse", "Teclado", "Monitor"];
+for(var i = 0; i < produtos.length; i++){
+    document.write(`<li style="color: purple">${produtos[i]}</li>`)
 }
+
+const musicas = [
+    "Bohemian Rhapsody - Queen",
+    "Imagine - John Lennon",
+    "Hotel California - Eagles",
+    "Smells Like Teen Spirit - Nirvana",
+    "Billie Jean - Michael Jackson"
+];
+
+for(var i = 0; i < musicas.length; i++){
+    document.write(`<li style="color: purple">${musicas[i]}</li>`)
+}
+
+
+//  1
+//  2
+//  3
+//  4
+//  5
+//  6
+//  7
+//  8
+//  9
+//  10
+
+
+
+
+
+
 
 // listaMusicasFesta.push(nomeMusica, nomeMusica1, nomeMusica2, nomeMusica3)
 // console.log(listaMusicasFesta);
