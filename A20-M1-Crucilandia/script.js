55bb7565f16ac3a7d4b27bf2df499a9437fbb6a8
Bia-source <bia_ferreirad@yahoo.com>
2025-02-05 19:45:17 -0300

    explicação laco while

---
+++ A20-M1-Crucilandia/script.js
@@ -0,0 +1,53 @@
+var listaMusicasFesta = [];
+
+// CODIGO ANTIGO
+// var nomeMusica = prompt("digite uma musica");
+// var nomeMusica1 = prompt("digite uma musica");
+// var nomeMusica2 = prompt("digite uma musica");
+// var nomeMusica3 = prompt("digite uma musica");
+
+// LAÇOS DE REPETIÇÃO -> ESTRUTURA DE CONTROLE
+// var count = 1;
+
+// // enquanto(condição for verdadeira){
+// //    faça isso 
+// // }
+// while (count < 5) {
+//     var nomeMusica = prompt("digite uma musica");
+//     listaMusicasFesta.push(nomeMusica);
+//     count = count + 1;
+// }
+// console.log(listaMusicasFesta)
+
+var listaNomes = ["Beatriz", "Kauanny", "Luisa"];
+console.log(listaNomes[0]);
+console.log(listaNomes[1]);
+console.log(listaNomes[2]);
+
+// condição -> quantidade de vezes que queremos 
+// repetir o mesmo código
+var vezes = 1;
+while(vezes < 4){
+    // código que queremos repetir 
+    console.log(listaNomes[0]);
+}
+
+
+
+
+
+
+
+
+// listaMusicasFesta.push(nomeMusica, nomeMusica1, nomeMusica2, nomeMusica3)
+// console.log(listaMusicasFesta);
+
+// // escrevendo no html a lista de musicas que o usuario inseriu
+// document.write(`<h1>${listaMusicasFesta[4]}</h1>`);
+
+// // document = HTML
+// // getElementById = buscando tag por id <li id="item1">
+// // .innerHTML = vai substituir o valor escrito dentro da tag
+// document.getElementById("item1").innerHTML = `${listaMusicasFesta[0]}`
+// document.getElementById("item2").innerHTML = `${listaMusicasFesta[1]}`
+// document.getElementById("item3").innerHTML = `${listaMusicasFesta[2]}`
