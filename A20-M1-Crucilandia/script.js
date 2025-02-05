4a0fb377514d6c262a9bca596f2536aa766d4cd1
Bia-source <bia_ferreirad@yahoo.com>
2025-02-05 20:04:29 -0300

    mais exemplos de while

--- A20-M1-Crucilandia/script.js
+++ A20-M1-Crucilandia/script.js
@@ -7,7 +7,7 @@ var listaMusicasFesta = [];
 // var nomeMusica3 = prompt("digite uma musica");
 
 // LAÇOS DE REPETIÇÃO -> ESTRUTURA DE CONTROLE
-// var count = 1;
+var count = 1;
 
 // // enquanto(condição for verdadeira){
 // //    faça isso 
@@ -20,24 +20,26 @@ var listaMusicasFesta = [];
 // console.log(listaMusicasFesta)
 
 var listaNomes = ["Beatriz", "Kauanny", "Luisa"];
-console.log(listaNomes[0]);
-console.log(listaNomes[1]);
-console.log(listaNomes[2]);
+// console.log(listaNomes[0]);
+// console.log(listaNomes[1]);
+// console.log(listaNomes[2]);
 
 // condição -> quantidade de vezes que queremos 
 // repetir o mesmo código
-var vezes = 1;
-while(vezes < 4){
+var vezes = 0;
+while(vezes < 3){
     // código que queremos repetir 
-    console.log(listaNomes[0]);
+    document.write(`incrivel divas ${listaNomes[vezes]} <br>`);
+    // atualização do valor vezes
+    vezes = vezes + 1;
 }
 
+var numero = 0;
 
-
-
-
-
-
+while(numero < 5){
+    document.write(`incrivel <br>`);
+    numero = numero + 1;
+}
 
 // listaMusicasFesta.push(nomeMusica, nomeMusica1, nomeMusica2, nomeMusica3)
 // console.log(listaMusicasFesta);
